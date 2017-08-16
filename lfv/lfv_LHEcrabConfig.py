51cc75c85933fc4f7e1a265aa2f61ebfbdb9bec2
TomCroote <tom.croote@gmail.com>
2017-08-16 13:45:23 +0200

    reverted changes

--- lfv/lfv_LHEcrabConfig.py
+++ lfv/lfv_LHEcrabConfig.py
@@ -21,13 +21,13 @@ config.JobType.scriptExe = './lfv_LHEGENSIMsubmissionScript.sh'
 
 config.Data.outputPrimaryDataset = 'LFV_ZToL1L2_13TeV_madgraph_pythia8'
 config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 10
+config.Data.unitsPerJob = 1000
 #NJOBS = 10  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = #NUMBEREVENTS#
 #config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
-config.Data.publication = False
-#config.Data.publication = True
+#config.Data.publication = False
+config.Data.publication = True
 config.Data.publishDBS = 'phys03'
 config.Data.outputDatasetTag = 'RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016'
 
