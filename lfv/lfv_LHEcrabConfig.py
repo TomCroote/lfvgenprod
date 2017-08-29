f7f73fb5b312a7779fd58c2afd820166ff3292f7
TomCroote <tom.croote@gmail.com>
2017-08-30 01:02:30 +0200

    Update lfv_LHEcrabConfig.py

--- lfv/lfv_LHEcrabConfig.py
+++ lfv/lfv_LHEcrabConfig.py
@@ -29,6 +29,6 @@ config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 #config.Data.publication = False
 config.Data.publication = True
 config.Data.publishDBS = 'phys03'
-config.Data.outputDatasetTag = 'RunIISummer16MiniAODv2-Croote17_80X_mcRun2_asymptotic_28_08_2017'
+config.Data.outputDatasetTag = 'RunIISummer16MiniAODv2-Croote17_80X_mcRun2_asymptotic_30_08_2017_1'
 
 config.Site.storageSite = "T2_DE_RWTH"
